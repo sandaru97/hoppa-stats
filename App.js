52aea2649f38008f9401c67f5c82c582a22c2761
sandaru97 <sandaruadithya@gmail.com>
2021-12-07 17:19:15 +1100

    table added

--- App.js
+++ App.js
@@ -19,19 +19,28 @@ class Table extends Component {
 
 
     render() {
-        var html=[];
+        color=true;
 
+        var html=[];
+var color="black";
         for(var i = 0; i < stats.length; i++) {
             var obj = stats[i];
         
 //html={html+<tr>{obj.key}</tr>};
-html.push(<tr  key={obj.key}>{obj.key}<td>{obj.value}</td></tr>);
-
+html.push(<tr style={{"padding":"5px","border": "none",background:(color?"#B4A793":"")}} key={obj.key}>{obj.key}<td>{obj.value}</td></tr>);
+color=!color;
         }
         //html=html+"</table>";
         return (
      <div>
-         <table>{html}</table>
+         <table 
+         cellspacing="0" cellpadding="0"
+         style={{
+             width:"100%",
+             "border": "none",
+             "font-size": "25px",
+         }}
+         id='stats'>{html}</table>
      </div>
         
         );
@@ -41,39 +50,49 @@ html.push(<tr  key={obj.key}>{obj.key}<td>{obj.value}</td></tr>);
 export default function App() {
     return (
         <View style={styles.container}>
+            <View style={{display: "flex","align-items":"center",justifyContent:"center",}}>
            <Text style={
                 styles.title
             }>Stats / Charts</Text>
-            <View style={{
-                wdith:"95%",
-
-            }}>
             <Chart
-  style={{ height: "40vw", width: "70vw" }}
+  style={{ height: 400, width: "100%", }}
   data={[
-    { x: 11/11/2011, y: 150 },
-    { x: 12/11/2011, y: 10 },
-    { x: 13/11/2011, y: 12 },
-    { x: 14/11/2011, y: 17 },
-    { x: 15/11/2011, y: 6 },
-    { x: 16/11/2011, y: 81 },
-    { x: 17/11/2011, y: 10 },
-    { x: 18/11/2011, y: 8 },
-    { x: 19/11/2011, y: 112 },
-    { x: 10/11/2011, y: 14 },
-    { x: 22/11/2011, y: 12 },
-    { x: 24/11/2011, y: 18 },
+    { x: -2, y: 15 },
+    { x: -1, y: 10 },
+    { x: 0, y: 12 },
+    { x: 1, y: 7 },
+    { x: 2, y: 6 },
+    { x: 3, y: 8 },
+    { x: 4, y: 10 },
+    { x: 5, y: 8 },
+    { x: 6, y: 12 },
+    { x: 7, y: 14 },
+    { x: 8, y: 12 },
+    { x: 9, y: 13.5 },
+    { x: 10, y: 18 },
   ]}
   //padding={{ left: 40, bottom: 20, right: 20, top: 20 }}
-  //xDomain={{ min: -2, max: 10 }}
- // yDomain={{ min: 0 }}
+  xDomain={{ min: -2, max: 10 }}
+  yDomain={{ min: 0, max: 20 }}
 >
   <VerticalAxis tickCount={11} theme={{ labels: { formatter: (v) => v.toFixed(2) } }} />
   <HorizontalAxis tickCount={5} />
-  <Area theme={{ gradient: { from: { color: '#eb9486' }, to: { color: '#ffa502', opacity: 0.4 } }}} />
-  <Line theme={{ stroke: { color: '#eb9486', width: 5 }, scatter: { default: { width: 4, height: 4, rx: 2 }} }} />
-</Chart></View>
-<Table></Table>
+  <Area theme={{ gradient: { from: { color: '#ffa502' }, to: { color: '#ffa502', opacity: 0.4 } }}} />
+  <Line theme={{ stroke: { color: '#ffa502', width: 5 }, scatter: { default: { width: 4, height: 4, rx: 2 }} }} />
+</Chart></View><br/>
+            <View style={{
+                wdith:"100%",
+                justifyContent:"center",
+
+            }}><br/>
+            <View 
+              style={{ flex:1,height: "40vw", width: "100%",position:"static", flex:1,}}
+              >
+        
+</View></View><br/>
+<View style={{paddingTop:"10px"}}>
+<Table style={{width:"100%",flex:1,}}></Table>
+        </View>
         </View>
     );
 }
@@ -81,6 +100,7 @@ export default function App() {
 const styles = StyleSheet.create({
    
     container: {
+        width:"90%",
         "border-bottom": "10px solid #eb9486",
         "border-top": "10px solid #eb9486",
         "height": "100%",
@@ -92,15 +112,17 @@ const styles = StyleSheet.create({
         display: "flex",
         flex: 1,
         // backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        "align-items": "center",
-        textAlign: 'center'
+        //alignItems: 'center',
+       // justifyContent: 'center',
+        //"align-items": "center",
+        textAlign: 'center',
+        justifyContent:"center",
+        display: "flex",
     },
     title: {
         position: "absolute",
-
         fontSize: "10vw",
-        top: "15px"
+        top: "15px",
+        display: "flex",
     },
 });
